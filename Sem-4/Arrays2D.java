46de062bdaaf1bef1a3dc2d173cf80dd8af4285c
Sushil <sushilksingh2001@gmail.com>
2025-02-21 18:36:12 +0530

    Add basic programs

---
+++ Sem-4/Arrays2D.java
@@ -0,0 +1,48 @@
+public class Arrays2D{
+    public static void main(String args[]){
+        //Printing using for each loop
+        // int b[][] = new int[3][3];
+        // for(int a[]:b){
+        //     for(int x: a){
+        //         System.out.print(x+" ");
+        //     }
+        //     System.out.println("");
+        // }
+
+        //Jagged Array(array with each row having a different length array)
+        // int[][] c = new int[3][];
+        // c[0] = new int[4];
+        // c[1] = new int[5];
+        // c[2] = new int[6];
+
+        // for(int a[]: c){
+        //     for(int d: a){
+        //         System.out.print(d+" ");
+        //     }
+        //     System.out.println("");
+        // }
+
+        //Matrix Multiplication
+        int[][] m1 = {{1,2,3},{3,6,4}};
+        int[][] m2 = {{2,5,4,2},{1,5,2,5},{5,3,1,4}};
+        int[][] m3 = new int[m1.length][m2[0].length];
+
+        for(int i=0; i<m1.length; i++){
+            for(int j=0; j<m2[0].length; j++){
+                for(int k=0; k<m2.length; k++){
+                    m3[i][j] += m1[i][k]*m2[k][j];
+                }
+            }
+        }
+
+        for(int i=0; i<m3.length; i++){
+            for(int j=0; j<m3[0].length; j++){
+                System.out.print(m3[i][j]+" ");
+            }
+            System.out.println("");
+        }
+
+        
+
+    }
+}
---
+++ Sem-4/gcd.java
@@ -0,0 +1,27 @@
+public class gcd {
+    static int calcGcd(int m, int n){
+        if(m == 0 || n == 0) {
+            System.out.println("GCD can't be calculated either of the numbers is zero");
+            return -1;
+        }
+        if(n<m){
+            int temp = n;
+            n=m;
+            m = temp;
+        }
+        int rem = 0;
+        do{
+            rem = n%m;
+            n = m;
+            m = rem;
+        }
+        while(rem != 0);
+
+        return n;
+    }
+
+    public static void main(String args[]){
+        int gcd = calcGcd(0,2);
+        System.out.println(gcd);
+    }
+}
